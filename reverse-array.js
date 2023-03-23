36bc3f4d0b44dcc8f09e6e4179518db4dab7bb1e
Yauheni Kazuseu <55188582+i0rdan@users.noreply.github.com>
2023-03-23 16:21:02 +0300

    Delete reverse-array.js

--- reverse-array.js
+++
@@ -1,7 +0,0 @@
-// https://www.codewars.com/kata/59b81886460387d8fc000043
-// No time for stories. Reverse an array, return the result. Do whatever you want with the original array. Don't use Array.prototype.reverse.
-// You have 30 bytes to spare.
-// Example: [1, 2, 3] → [3, 2, 1]
-// require isn't allowed as well.
-
-reverse=a=>a.map(a.pop,[...a])
